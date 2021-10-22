89cd1f3fb50e9781e28713a5aa3afa75f3722b71
PrestaSafe <guillaume.batier@gmail.com>
2021-10-22 15:45:29 +0200

    pagination

--- app/Http/Livewire/Postscrud.php
+++ app/Http/Livewire/Postscrud.php
@@ -4,27 +4,34 @@ namespace App\Http\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Postscrud extends Component
 {
-    public $posts;
+    // use WithPagination;
+    // public $posts;
     public $search = '';
-
-
+    // protected $paginationTheme = 'bootstrap';
+    
+    // posts
+    public $id_post = '';
+    public $title = '';
+    public $content = '';
+ 
     public function render()
     {
-        $this->getPosts();
-        return view('livewire.postscrud');
+        $posts = $this->getPosts();
+        return view('livewire.postscrud',['posts' => $posts]);
     }
 
     public function getPosts()
     {
-        $posts =
-        (empty($this->search)) ?
-        Post::all() : 
-        Post::where('title','like',"%$this->search%")->get();
+        $posts = 
+        ($this->search == '') 
+        ? Post::paginate(20)
+        : Post::where('title','like',"%$this->search%")->paginate(20);
     
-        $this->posts = $posts;  
+        return $posts;  
     }
 
     public function delete($id)
@@ -32,4 +39,29 @@ class Postscrud extends Component
         Post::destroy($id);
         $this->getPosts();
     }
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        $this->title = $post->title;
+        $this->content = $post->content;
+        $this->id_post = $post->id;
+
+    }
+
+    public function save()
+    {
+        $post = Post::find($this->id_post);
+        $post->title = $this->title;
+        $post->content = $this->content;
+        $post->save();
+
+        $this->reset(['id_post','title','content']);
+
+
+    } 
 }
--- resources/views/livewire/postscrud.blade.php
+++ resources/views/livewire/postscrud.blade.php
@@ -2,27 +2,62 @@
     <input type="text" wire:model.debounce.200ms="search" class="form-control" placeholder="Rechercher..">
     <table class="table">
         <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Title</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-        </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
         </thead>
         <tbody>
             @forelse ($posts as $post)
-            <tr class="post-line">
-                <th scope="row">{{ $post->id }}</th>
-                <td>{{ $post->title }}</td>
-                <td> <a class="text-primary" href="{{ route('posts.edit',$post) }}">Editer</a> </td>
-                <td>
-                    <button wire:click="delete({{$post->id}})" type="button" class="btn btn-primary">Delete</button>
-                </td>
-            </tr>
-            @empty    
-            <tr><th colspan="5">Aucun articles</th></tr>
+                <tr class="post-line">
+                    <th scope="row">{{ $post->id }}</th>
+                    <td>{{ $post->title }}</td>
+                    <td> <a class="text-primary" 
+                        wire:click="edit({{$post->id}})"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        href="{{ route('posts.edit', $post) }}">Editer</a> </td>
+                    <td>
+                        <button wire:click="delete({{ $post->id }})" type="button"
+                            class="btn btn-primary">Delete</button>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <th colspan="5">Aucun articles</th>
+                </tr>
             @endforelse
-        
+            
+            <!-- Modal -->
+            <div wire:ignore.self class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Post Title</label>
+                                <input type="text" class="form-control" id="title" wire:model="title" placeholder="name@example.com">
+                              </div>
+                              <div class="mb-3">
+                                <label for="content" class="form-label">Content</label>
+                                <textarea class="form-control" id="content" wire:model="content" rows="3"></textarea>
+                              </div>
+                              {{-- <input type="text" wire:model="id_post"> --}}
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" wire:click="save" data-bs-dismiss="modal">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </tbody>
     </table>
+    {{ $posts->links() }}
 </div>
